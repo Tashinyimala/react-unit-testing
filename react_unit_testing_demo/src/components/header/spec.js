7c4925e6387b980b32a9d91532428913bf21aa41
Tashi Nyima <tashinyima@hotmail.com>
2019-03-24 22:31:57 +0000

    header component and test

---
+++ react_unit_testing_demo/src/components/header/spec.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Header from './Header';
+import { findByTestAttr } from './../../../../utils/utils';
+
+const setUp = (props = {}) => shallow(<Header {...props} />);
+
+describe('Header Component', () => {
+  let component;
+  beforeEach(() => {
+    component = setUp();
+  });
+
+  test('Should render without errors', () => {
+    // console.log(component.debug());
+    const wrapper = findByTestAttr(component, 'headerComponent');
+    expect(wrapper.length).toBe(1);
+  });
+
+  test('Should render a logo', () => {
+    const logo = findByTestAttr(component, 'logoImg');
+    expect(logo.length).toBe(1);
+  });
+});
